712031286d8567326b79c2fa9e2c44d0ed264403
Deepak <dbehera647@gmail.com>
2023-12-13 12:11:36 +0530

    Creating the HomePage

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,17 +5,17 @@ const Navbar = () => {
    <>
    <header>
     <div className="flex items-center justify-between p-6">
-        <div className="text-lg">
-            <Link to="/">Wonder World</Link>
+        <div className="text-2xl">
+            <Link to="/">Beyond<span className="text-violet-950">Tech</span></Link>
         </div>
         <nav>
             <ul className="flex list-none gap-2">
-                <li className="px-2 text-lg"><Link to="/">Home</Link></li>
-                <li className="px-2 text-lg"><Link to="/about">About</Link></li>
-                <li className="px-2 text-lg"><Link to="/service">Services</Link></li>
-                <li className="px-2 text-lg"><Link to="/contact">Contact</Link></li>
-                <li className="px-2 text-lg"><Link to="/register">Register</Link></li>
-                <li className="px-2 text-lg"><Link to="/login">Log In</Link></li>
+                <li className="px-2 text-lg"><Link to="/" className="focus:text-violet-950 focus:text-xl">Home</Link></li>
+                <li className="px-2 text-lg"><Link to="/about" className="focus:text-violet-950 focus:text-xl">About</Link></li>
+                <li className="px-2 text-lg"><Link to="/service" className="focus:text-violet-950 focus:text-xl">Services</Link></li>
+                <li className="px-2 text-lg"><Link to="/contact" className="focus:text-violet-950 focus:text-xl">Contact</Link></li>
+                <li className="px-2 text-lg"><Link to="/register" className="focus:text-violet-950 focus:text-xl">Register</Link></li>
+                <li className="px-2 text-lg"><Link to="/login" className="focus:text-violet-950 focus:text-xl">Log In</Link></li>
             </ul>
         </nav>
     </div>
